c994574b34fbba426f2142c35e3826932a613df3
olawa001 <abdullateefyusuf80@gmail.com>
2024-12-30 08:52:38 +0100

    chart

--- loan_predict.py
+++ loan_predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -9,7 +10,28 @@ print(df.head())
 print(df.columns)
 print(df.describe())
 
-df['Gender'].fillna(df['Gender'].mode()[0], inplace=True)
-df['Married'].fillna(df['Married'].mode()[0], inplace=True)
-df['Dependents'].fillna(df['Dependents'].mode()[0], inplace=True)
-df['Self_Employed'].fillna(df['Self_Employed'].mode()[0], inplace=True)
+df.fillna({'Gender': 'Gender'}, inplace=True)
+df.fillna({'Married': 'Married'}, inplace=True)
+df.fillna({'Dependent': 'Dependent'}, inplace=True)
+df.fillna({'Self_Employed': 'Self_Employed'}, inplace=True)
+
+df.fillna({'LoanAmount': 'LoanAmount'}, inplace=True)
+df.fillna({'Loan_Amount_Term': 'Loan_Amount_Term'}, inplace=True)
+df.fillna({'Credit_History': 'Credit_History'}, inplace=True)
+
+loan_status_count = df['Loan_Status'].value_counts()
+fig_loan_status = px.pie(
+    loan_status_count,
+    names=loan_status_count.index,
+    title='Loan Approval Status'
+)
+fig_loan_status.show()
+
+gender_status = df['Gender'].value_counts()
+fig_gender = px.bar(
+    gender_status,
+    x=gender_status.index,
+    y=gender_status.values,
+    title='Gender Distribution'
+)
+fig_gender.show()
